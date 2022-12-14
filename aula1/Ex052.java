6ded165f35cda5484a3d738c1085dc0ac54c6c05
matheusqq233 <117654021+matheusqq233@users.noreply.github.com>
2022-12-14 11:28:42 -0300

    Add files via upload

--- aula1/Ex052.java
+++ aula1/Ex052.java
@@ -1,5 +1,4 @@
-package aula1;
-
+package aulas;
 import java.util.Scanner;
 public class Ex052 {
     public static void main(String[] args) {
@@ -13,10 +12,21 @@ public class Ex052 {
         System.out.print("SALTO: ");
         int salto = read.nextInt();
         
-        for(int i = 0;inicio <= fim ;i++){
-            System.out.printf(" %d ",inicio);
-            inicio = inicio + salto;
+        while(salto < 0){
+            System.out.print("\n****Resposta inválida ****\n");
+            System.out.print("Salto: ");
+            salto = read.nextInt();
         }
-    }
+           if(inicio<=fim){
+            for(int i = inicio; i<=fim;i+=salto){
+            System.out.printf(" %d ",i);
+       }
+       }else{
+               for(int e = inicio;e>=fim;e-=salto){
+                   System.out.printf(" %d ",e);
+               }
+           }
 }
+}
+       
 
