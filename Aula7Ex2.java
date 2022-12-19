1554dd6abb8c922b263ac2f7d58a3c99f6f4bc15
matheusqq233 <117654021+matheusqq233@users.noreply.github.com>
2022-12-19 10:54:04 -0300

    Add files via upload

---
+++ Aula7Ex2.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class Aula7Ex2 {
+    public static void main(String[] args) {
+        
+        for(int i = 100;i>=0;i-=10){
+            System.out.print(i + " ");
+    }    
+        System.out.print("\nAcabou!!");
+   }
+}
